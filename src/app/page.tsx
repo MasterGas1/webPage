0ecd8484446fce1d5b32523922c3bff5c23b9369
ManuelBarba06 <113741996+ManuelBarba06@users.noreply.github.com>
2023-11-05 21:33:04 -0600

    Add icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import { Button } from '@nextui-org/react'
 
+import Image from 'next/image'
+
+import Example from '../svg/example.svg'
+
 export default function Page() {
+
   return (
     <div>
-        <Button className='bg-slate-400' color='primary' variant='bordered'>Click me</Button>
+        <Button className='bg-slate-400' color='primary' variant='bordered'>
+          <Image
+            src={Example}
+            alt='Example'
+          />
+        </Button>
     </div>
   )
 }
+
