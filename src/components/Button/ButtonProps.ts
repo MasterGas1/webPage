d1bb33c538ed9ef71fc08ba81952296b4c334ac1
AlexeiSan <alexei_sanchez@avantech.com.mx>
2025-04-15 18:08:06 -0700

    Button componenet and added to storybook

--- src/components/Button/ButtonProps.ts
+++ src/components/Button/ButtonProps.ts
@@ -1,13 +1,22 @@
 import { ButtonHTMLAttributes, ReactNode, MouseEvent } from "react";
+import { CSSProperties } from 'react'
+import { IconType } from 'react-icons'
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  label: string;
-  variants?: "flat" | "bordered";
-  backgroundColor?: "principal" | "secondary" | "error";
-  radius?: "none" | "full" | "medium";
-  isLoading?: boolean;
-  disabled?: boolean;
-  leftIcon?: ReactNode;
-  rightIcon?: ReactNode;
+  label?: string
+  type?: "button" | "submit"
+  variant?: "bordered" | "flat"
+  backgroundColor?: "bgPrimary" | "bgSecondary" | "bgError"
+  //Enum
+  textColor?: "textLight" | "textDark" | "textPrimary" | "textSecondary"
+  style?: CSSProperties
+  hover?: CSSProperties
+  icon?: IconType
+  iconPosition?: "left" | "right"
+  disabled?: boolean
+  size?: "small" | "medium" | "large"
+  fullWidth?: boolean
+  isLoading?: boolean
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+  radius?: "full" | "large" | "medium" | "small" | "none"
 }
